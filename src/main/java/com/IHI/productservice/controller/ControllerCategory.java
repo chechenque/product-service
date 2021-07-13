1876e6961aa2e64c0282d8b69add281fb98dcbed
Chechenque <43196753+chechenque@users.noreply.github.com>
2021-07-13 10:41:08 -0500

    Practica 2 Terminada

--- src/main/java/com/IHI/productservice/controller/ControllerCategory.java
+++ src/main/java/com/IHI/productservice/controller/ControllerCategory.java
@@ -16,59 +16,64 @@ import com.IHI.productservice.dto.Category;
 import com.IHI.productservice.exceptionHandling.ApiException;
 import com.IHI.productservice.service.ServiceCategory;
 
-@RestController
+@RestController 
 @RequestMapping("/category")
 public class ControllerCategory {
 
-	@Autowired
-	ServiceCategory serviceCategory;
-	
-	@GetMapping
-	public ResponseEntity<Object> getCategorys() throws Exception{
-		try {
-			return new ResponseEntity<>(serviceCategory.getCategorys(),HttpStatus.OK);
-		}catch(Exception e) {
-			throw new ApiException(HttpStatus.NOT_FOUND,e.getLocalizedMessage());
-		}
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Object> getCategory(@PathVariable("id") int id) throws Exception{
-		try {
-			return new ResponseEntity<>(serviceCategory.getCategory(id), HttpStatus.OK);
-		}catch(Exception e){
-			throw new ApiException(HttpStatus.NOT_FOUND,e.getLocalizedMessage());
-		}
-		
-	}
-	
-	@PostMapping
-	public ResponseEntity<Object> createCategory(@RequestBody Category category) throws Exception{
-		try {
-			serviceCategory.createCategory(category);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}catch(Exception e) {
-			throw new ApiException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
-		}
-	}
-	
-	@PutMapping("/{id}")
-	public ResponseEntity<Object> updateCategory(@RequestBody Category category, @PathVariable("id") int id) throws Exception{
-		try {
-			serviceCategory.updateCategory(category, id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}catch(Exception e) {
-			throw new ApiException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
-		}
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> deleteCategory(@PathVariable("id") int id) throws Exception{
-		try {
-			serviceCategory.deleteCategory(id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		}catch(Exception e) {
-			throw new ApiException(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
-		}
-	}
+  @Autowired
+  ServiceCategory serviceCategory;
+  
+  @GetMapping
+  public ResponseEntity<Object> getCategorys() throws Exception{
+    try {
+      return new ResponseEntity<>(serviceCategory.getCategorys(),HttpStatus.OK);
+    }catch(Exception e) {
+      throw new ApiException(HttpStatus.NOT_FOUND,e.getLocalizedMessage());
+    }
+  }
+  
+  @GetMapping("/{id}")
+  public ResponseEntity<Object> getCategory(@PathVariable("id") int id) throws Exception{
+    try {
+      return new ResponseEntity<>(serviceCategory.getCategory(id), HttpStatus.OK);
+    }catch(Exception e){
+      throw new ApiException(HttpStatus.NOT_FOUND,e.getLocalizedMessage());
+    }
+    
+  }
+  
+  @PostMapping
+  public ResponseEntity<Object> createCategory(@RequestBody Category category) throws Exception{
+    try {
+      serviceCategory.createCategory(category);
+      return new ResponseEntity<>(HttpStatus.OK);
+    }catch(Exception e) {
+      throw new ApiException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+    }
+  }
+  
+  @PutMapping("/{id}")
+  public ResponseEntity<Object> updateCategory(@RequestBody Category category, @PathVariable("id") int id) throws Exception{
+    try {
+      serviceCategory.updateCategory(category, id);
+      return new ResponseEntity<>(HttpStatus.OK);
+    }catch(Exception e) {
+      System.out.println(e.getLocalizedMessage());
+      if(e.getLocalizedMessage().equals("La categoria No existe")) {
+        throw new ApiException(HttpStatus.NOT_FOUND,e.getLocalizedMessage());
+      }else {
+        throw new ApiException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+      }
+    }
+  }
+  
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Object> deleteCategory(@PathVariable("id") int id) throws Exception{
+    try {
+      serviceCategory.deleteCategory(id);
+      return new ResponseEntity<>(HttpStatus.OK);
+    }catch(Exception e) {
+      throw new ApiException(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
+    }
+  }
 }
